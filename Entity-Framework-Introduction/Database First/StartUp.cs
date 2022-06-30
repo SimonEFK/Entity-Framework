4e28b24f1104dd1bd56b5eabd49a27e0e2a1dedd
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-06-30 06:21:34 +0300

    GetEmployeesFullInformation

--- Entity-Framework-Introduction/Database First/StartUp.cs
+++ Entity-Framework-Introduction/Database First/StartUp.cs
@@ -16,6 +16,29 @@ namespace SoftUni
                
         }
 
-        
+        //03
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => new
+            {
+                x.EmployeeId,
+                x.FirstName,
+                x.LastName,
+                x.MiddleName,
+                x.JobTitle,
+                x.Salary
+            }).ToList();
+            var sb = new StringBuilder();
+
+            foreach (var item in employees.OrderBy(x => x.EmployeeId))
+            {
+                sb.AppendLine($"{item.FirstName} {item.LastName} {item.MiddleName} {item.JobTitle} {item.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+
     }
 }
