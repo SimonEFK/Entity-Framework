1964a9df4cf4c82a1330ac9eb7808b3d9843adae
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-06-30 06:15:25 +0300

    Database First

---
+++ Entity-Framework-Introduction/Database First/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Database_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
