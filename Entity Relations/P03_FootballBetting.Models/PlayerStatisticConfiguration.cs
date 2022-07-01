651d92dd5e53b2a5ab592d3cf21ed1b59dab99b1
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-07-01 20:34:34 +0300

    P03

---
+++ Entity Relations/P03_FootballBetting.Models/PlayerStatisticConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Models
+{
+    public class PlayerStatisticConfiguration : IEntityTypeConfiguration<PlayerStatistic>
+    {
+        public void Configure(EntityTypeBuilder<PlayerStatistic> builder)
+        {
+            builder.HasKey(x => new { x.GameId, x.PlayerId });
+        }
+    }
+}
---
+++ Entity Relations/P03_FootballBetting.Models/TeamConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Models
+{
+    public class TeamConfiguration : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> builder)
+        {
+            builder.HasOne(x => x.PrimaryKitColor)
+                .WithMany(x => x.PrimaryKitTeams)
+                .HasForeignKey(x => x.PrimaryKitColorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.SecondaryKitColor)
+                .WithMany(x => x.SecondaryKitTeams)
+                .HasForeignKey(x => x.SecondaryKitColorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+
+
+        }
+    }
+}
