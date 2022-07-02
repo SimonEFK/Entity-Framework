d182d85b84cd782089e3a3cec330a92033c227cf
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-07-02 11:51:28 +0300

    P01_HospitalDatabase

---
+++ Entity Relations/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext:DbContext
+    {
+        public HospitalContext()
+        {
+
+        }
+
+        public HospitalContext( DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+        public DbSet<Visitation> Visitations { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured==false)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=	Hospital;Integrated Security=True;");
+            }
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfiguration(new DiagnoseConfiguration());
+            modelBuilder.ApplyConfiguration(new MedicamentConfiguration());
+            modelBuilder.ApplyConfiguration(new PatientConfiguration());
+            modelBuilder.ApplyConfiguration(new PatientMedicamentConfiguration());
+            modelBuilder.ApplyConfiguration(new VisitationConfiguration());
+
+
+
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Entity Relations/P01_HospitalDatabase/Data/Models/PatientConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.HasKey(x=>x.PatientId);
+
+            builder
+                .HasMany(x=>x.Visitations)
+                .WithOne(x=>x.Patient)
+                .HasForeignKey(x=>x.PatientId);
+
+            builder
+                .HasMany(x => x.Diagnoses)
+                .WithOne(x => x.Patient)
+                .HasForeignKey(x => x.PatientId);
+
+
+            builder
+                .Property(x => x.FirstName)
+                .IsUnicode()
+                .HasMaxLength(50);
+
+            builder
+                .Property(x => x.LastName)
+                .IsUnicode()
+                .HasMaxLength(50);
+
+            builder
+                .Property(x => x.Address)
+                .IsUnicode()
+                .HasMaxLength(250);
+
+            builder
+                .Property(x => x.Email)
+                .IsUnicode(false)
+                .HasMaxLength(80);
+
+        }
+    }
+}
