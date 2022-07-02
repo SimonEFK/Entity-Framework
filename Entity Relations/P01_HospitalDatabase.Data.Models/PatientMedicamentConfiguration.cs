640ef3444efb11ca8d2b3975ca9d72d1a7b40eb4
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-07-02 12:36:24 +0300

    Hospital Database

---
+++ Entity Relations/P01_HospitalDatabase.Data.Models/PatientMedicamentConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class PatientMedicamentConfiguration : IEntityTypeConfiguration<PatientMedicament>
+    {
+        public void Configure(EntityTypeBuilder<PatientMedicament> builder)
+        {
+            builder.HasKey(x=> new {x.PatientId,x.MedicamentId});
+
+            builder
+                .HasOne(x => x.Patient)
+                .WithMany(x => x.Prescriptions)
+                .HasForeignKey(x => x.PatientId);
+
+            builder
+                .HasOne(x => x.Medicament)
+                .WithMany(x => x.Prescriptions)
+                .HasForeignKey(x => x.MedicamentId);
+
+
+        }
+    }
+}
