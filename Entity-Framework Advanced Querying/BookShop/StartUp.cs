9dde8581ff76d408e84a6b3b50be22317ae1f6f8
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-07-06 15:25:11 +0300

    BookShop System + ⦁	Age Restriction Method

---
+++ Entity-Framework Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using BookShop.Models.Enums;
+
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            
+            string inpuLine = Console.ReadLine();
+            var result = GetBooksByAgeRestriction(db, inpuLine);
+            Console.WriteLine(result);
+
+
+        }
+
+
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageGroup = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .ToList()
+                .Where(x => x.AgeRestriction == ageGroup)
+                .Select(x=>new
+                {
+                    x.Title
+                    
+                })
+                .OrderBy(x => x.Title);
+
+
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Join(Environment.NewLine, books.Select(x => x.Title)));
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
