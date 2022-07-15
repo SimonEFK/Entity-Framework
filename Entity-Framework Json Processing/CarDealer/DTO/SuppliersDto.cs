5093872a31190d23984afb1be4f6640ccbe6965e
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-07-15 09:10:51 +0300

    Query 9. Import Suppliers

---
+++ Entity-Framework Json Processing/CarDealer/DTO/SuppliersDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO
+{
+    public class SuppliersDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("isImporter")]
+        public bool IsImporter { get; set; }
+
+    }
+}
---
+++ Entity-Framework Json Processing/CarDealer/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var carDealerContext = new CarDealerContext();
+            ResetDatabase(carDealerContext);
+
+            var importSuppliersJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+
+            Console.WriteLine(ImportSuppliers(carDealerContext, importSuppliersJson));
+
+        }
+
+        private static void ResetDatabase(CarDealerContext carDealerContext)
+        {
+            carDealerContext.Database.EnsureDeleted();
+            Console.WriteLine("Deleted");
+            carDealerContext.Database.EnsureCreated();
+            Console.WriteLine("Created");
+        }
+
+        private static IMapper InitializeMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            });
+            IMapper mapper = config.CreateMapper();
+            return mapper;
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var mapper = InitializeMapper();
+
+            var suppliersDesereliazed = JsonConvert.DeserializeObject<IEnumerable<SuppliersDto>>(inputJson);
+
+            var suppliers = mapper.Map<IEnumerable<Supplier>>(suppliersDesereliazed);
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            ;
+
+            return $"Successfully imported {suppliers.Count()}.";
+
+        }
+    }
+
+
+
+
+}
