af8f9eb57915b5faee551fedc637809f3dcb5d17
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2022-07-18 12:39:00 +0300

    Import Cars

---
+++ Entity-Framework Json Processing/CarDealer/DTO/CarImportModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO
+{
+    public class CarImportModel
+    {
+        [JsonProperty("make")]
+        public string Make { get; set; }
+
+        [JsonProperty("model")]
+        public string Model { get; set; }
+
+        [JsonProperty("travelledDistance")]
+        public long TravelledDistance { get; set; }
+
+        public int[] PartsId { get; set; }
+        //[JsonProperty("partsId")]
+        //public CarPartsImportModel[] PartsId { get; set; }
+
+    }
+}
+
+/*
+ {
+    "make": "Opel",
+    "model": "Omega",
+    "travelledDistance": 176664996,
+    "partsId": [
+      38,
+      102,
+      23,
+      116,
+      46,
+      68,
+      88,
+      104,
+      71,
+      32,
+      114
+    ]
+  }
+ */
+
+
--- Entity-Framework Json Processing/CarDealer/StartUp.cs
+++ Entity-Framework Json Processing/CarDealer/StartUp.cs
@@ -18,15 +18,59 @@ namespace CarDealer
             //ResetDatabase(carDealerContext);
 
             //var importSuppliersJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+            //ImportSuppliers(carDealerContext, importSuppliersJson);
 
-            //Console.WriteLine(ImportSuppliers(carDealerContext, importSuppliersJson));
+            //var importPartsJson = File.ReadAllText(@"../../../Datasets/parts.json");
 
-            var importPartsJson = File.ReadAllText(@"../../../Datasets/parts.json");
+            //ImportParts(carDealerContext, importPartsJson);
 
-            Console.WriteLine(ImportParts(carDealerContext, importPartsJson));
-
-
+            var carsImportJson = File.ReadAllText(@"./Datasets/cars.json");
+            Console.WriteLine(ImportCars(carDealerContext, carsImportJson));
+            ;
         }
+
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var cars = JsonConvert.DeserializeObject<IEnumerable<CarImportModel>>(inputJson);
+
+
+            var dbCars = new List<Car>();
+            var dbCarParts = new List<PartCar>();
+            var validParts = context.Parts.Select(x => x.Id).ToList();
+
+            foreach (var currentCar in cars)
+            {
+                var car = new Car
+                {
+                    Make = currentCar.Make,
+                    Model = currentCar.Model,
+                    TravelledDistance = currentCar.TravelledDistance,
+                    PartCars = new List<PartCar>()
+                };
+
+                foreach (var part in currentCar.PartsId.Distinct())
+                {
+                    if (validParts.Contains(part))
+                    {
+                        car.PartCars.Add(new PartCar
+                        {
+                            PartId = part,
+                            CarId = car.Id
+                        });
+                    }
+                }
+                dbCars.Add(car);
+
+            }
+            context.AddRange(dbCars);
+            context.SaveChanges();
+
+
+
+            return $"Successfully imported {dbCars.Count}.";
+        }
+
 
         private static void ResetDatabase(CarDealerContext carDealerContext)
         {
@@ -76,8 +120,8 @@ namespace CarDealer
             var partsDesereliazed = JsonConvert.DeserializeObject<IEnumerable<Part>>(inputJson);
 
             var validSuppliers = context.Suppliers.Select(x => x.Id).ToList();
-            var validParts = partsDesereliazed.Where(x => validSuppliers.Contains(x.SupplierId)).ToList() ;
-            
+            var validParts = partsDesereliazed.Where(x => validSuppliers.Contains(x.SupplierId)).ToList();
+
             context.AddRange(validParts);
             context.SaveChanges();
 
